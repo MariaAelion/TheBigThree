a366b7254f0d6b4da3f1504e6eff16419a13c6d6
Loucas31 <lucasdubois.work@gmail.com>
2019-02-26 15:28:06 +0100

    Creation Utilisateur + verifUserbyID

--- src/main/java/com/todoitproject/service/impl/GlobalService.java
+++ src/main/java/com/todoitproject/service/impl/GlobalService.java
@@ -1,10 +1,12 @@
 package com.todoitproject.service.impl;
 
+import java.util.Optional;
+
 import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
+import com.todoitproject.persistence.entity.EUser;
 import com.todoitproject.persistence.repository.ProjectRepository;
 import com.todoitproject.persistence.repository.TaskRepository;
 import com.todoitproject.persistence.repository.UserRepository;
@@ -17,5 +19,20 @@ public class GlobalService implements IGlobalService {
 	@Autowired ProjectRepository projectRepository;
 	@Autowired TaskRepository taskRepository;
 	@Autowired UserRepository userRepository;
+	
+	
+	
+	private EUser getUserByLog(long id) {
+		
+		Optional<EUser> oeUser = userRepository.findUserById(id);
+		
+		if (oeUser.isPresent()) {
+			return oeUser.get();
+			
+		} else {
+			throw new com.todoitproject.exception.NotFoundException ("Cet utilisateur n'existe pas");
+		}
+		
+	}
 
 }
