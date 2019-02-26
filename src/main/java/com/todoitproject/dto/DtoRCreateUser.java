2463a3bd64240e972d92054905c68568c850dec6
Loucas31 <lucasdubois.work@gmail.com>
2019-02-26 15:07:57 +0100

    a

---
+++ src/main/java/com/todoitproject/dto/DtoRCreateUser.java
@@ -0,0 +1,16 @@
+package com.todoitproject.dto;
+
+public class DtoRCreateUser {
+	
+	private boolean confirm;
+
+	public boolean isConfirm() {
+		return confirm;
+	}
+
+	public void setConfirm(boolean confirm) {
+		this.confirm = confirm;
+	}
+	
+
+}
--- src/main/java/com/todoitproject/service/ILogService.java
+++ src/main/java/com/todoitproject/service/ILogService.java
@@ -1,5 +1,10 @@
 package com.todoitproject.service;
 
+import com.todoitproject.dto.DtoCreateUser;
+import com.todoitproject.dto.DtoRCreateUser;
+
 public interface ILogService {
+
+	DtoRCreateUser createUser(DtoCreateUser dtoCreateUser);
 
 }
