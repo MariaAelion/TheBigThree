83ab56768a2f8affe3f94c7c8b0e61e5dd7dd7b1
JigSaw31 <34063659+JigSaw31@users.noreply.github.com>
2019-03-01 14:48:25 +0100

    Relations bidirectionnelle

--- src/main/java/com/todoitproject/persistence/entity/EProject.java
+++ src/main/java/com/todoitproject/persistence/entity/EProject.java
@@ -1,5 +1,8 @@
 package com.todoitproject.persistence.entity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -7,8 +10,11 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
+
+import org.hibernate.annotations.Cascade;
+import org.hibernate.annotations.CascadeType;
 
 @Entity
 @Table(name="t_projet")
@@ -29,6 +35,9 @@ public class EProject {
 	@ManyToOne
 	@JoinColumn(name="id_user", nullable=true)
 	private EUser eUser;
+	
+	@OneToMany(mappedBy="eProject", cascade = javax.persistence.CascadeType.ALL)
+	private List<ETask> taches = new ArrayList<ETask>();
 
 	/**
 	 * @return the id
